ee8afdbccae4e960fb57f5f7cba81cd0e893d1d1
yb kim <qhd0084@gmail.com>
2023-03-03 16:24:40 +0900

    Feature: NextAuth Authentication with firebase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,36 @@
 import Sidebar from "@/components/Sidebar";
 import "@/styles/globals.css";
+import SessionProvider from "@/components/SessionProvider";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import Login from "@/components/Login";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
+
   return (
-    <html lang="en">
+    <html>
       <head />
       <body>
-        <div className="flex">
-          <div className="bg-gray-900 max-w-xs h-screen overflow-auto md:min-w-[20rem]">
-            <Sidebar />
-          </div>
-          {/* ClientProvider */}
-          <div className="flex-1 bg-slate-800">{children}</div>
-        </div>
+        <SessionProvider session={session}>
+          {!session ? (
+            <Login />
+          ) : (
+            <div className="flex">
+              <div className="bg-gray-900 max-w-xs h-screen overflow-y-auto md:min-w-[20rem]">
+                <Sidebar />
+              </div>
+
+              {/* ClientProvider */}
+
+              <div className="flex-1 bg-slate-800">{children}</div>
+            </div>
+          )}
+        </SessionProvider>
       </body>
     </html>
   );
