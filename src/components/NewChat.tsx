ef646d3a43dc5fa185f5ab0df9682ff59f74651e
yb kim <qhd0084@gmail.com>
2023-03-07 21:22:36 +0900

    Feature: NewChat button
    create new collection into firestore

--- src/components/NewChat.tsx
+++ src/components/NewChat.tsx
@@ -1,8 +1,32 @@
+"use client";
 import { PlusIcon } from "@heroicons/react/24/outline";
+import { db } from "@/firebase/firebase";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const NewChat = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const createNewChat = async () => {
+    const doc = await addDoc(
+      collection(db, "users", session?.user?.email!, "chats"),
+      {
+        messages: [],
+        userId: session?.user?.email,
+        createdAt: serverTimestamp(),
+      }
+    );
+
+    router.push(`/chat/${doc.id}`);
+  };
+
   return (
-    <div className="flex border-gray-700 border chat-row">
+    <div
+      onClick={createNewChat}
+      className="flex border-gray-700 border chat-row"
+    >
       <PlusIcon className="w-4 h-4" />
       <p>New Chat</p>
     </div>
