4d18676a66ea63afa8d429c45bdefc66643da8b7
yb kim <qhd0084@gmail.com>
2023-03-07 22:30:35 +0900

    Feature: ChatRow
    show created chat and delete chat

---
+++ src/app/chat/[id]/page.tsx
@@ -0,0 +1,9 @@
+const ChatPage = () => {
+  return (
+    <div>
+      hello<h1>hey</h1>
+    </div>
+  );
+};
+
+export default ChatPage;
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,11 +1,24 @@
 "use client";
-import { signOut, useSession } from "next-auth/react";
-import Image from "next/image";
-import NewChat from "./NewChat";
-import SignOut from "./SignOut";
+import { useSession } from "next-auth/react";
+import NewChat from "@/components/NewChat";
+import SignOut from "@/components/SignOut";
+import ChatRow from "@/components/ChatRow";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { collection, orderBy, query } from "firebase/firestore";
+import { db } from "@/firebase/firebase";
 
 const Sidebar = () => {
   const { data: session, status } = useSession();
+
+  const [chats, loading, error] = useCollection(
+    session &&
+      query(
+        collection(db, "users", session.user?.email!, "chats"),
+        orderBy("createdAt", "asc")
+      )
+  );
+
+  console.log(chats);
 
   return (
     <div className="p-2 flex flex-col h-screen">
@@ -15,6 +28,9 @@ const Sidebar = () => {
           <div>{/* ModelSelection */}</div>
 
           {/* Map through the chatRows */}
+          {chats?.docs.map((chat) => (
+            <ChatRow key={chat.id} id={chat.id} />
+          ))}
         </div>
       </div>
 
