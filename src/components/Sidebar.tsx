aec9800749b9c99e8f90d97de30def78ff168d24
yb kim <qhd0084@gmail.com>
2023-03-09 21:53:45 +0900

    UI: ModelSelection

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,6 +3,7 @@ import { useSession } from "next-auth/react";
 import NewChat from "@/components/NewChat";
 import SignOut from "@/components/SignOut";
 import ChatRow from "@/components/ChatRow";
+import ModelSelection from "@/components/ModelSelection";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { collection, orderBy, query } from "firebase/firestore";
 import { db } from "@/firebase/firebase";
@@ -23,12 +24,23 @@ const Sidebar = () => {
       <div className="flex-1">
         <div>
           <NewChat />
-          <div>{/* ModelSelection */}</div>
 
-          {/* Map through the chatRows */}
-          {chats?.docs.map((chat) => (
-            <ChatRow key={chat.id} id={chat.id} />
-          ))}
+          <div className="hidden sm:inline">
+            <ModelSelection />
+          </div>
+
+          <div className="flex flex-col space-y-2 my-2">
+            {loading && (
+              <div className="animation-pulse text-center text-white">
+                <p>Loading Chats...</p>
+              </div>
+            )}
+
+            {/* Map through the chatRows */}
+            {chats?.docs.map((chat) => (
+              <ChatRow key={chat.id} id={chat.id} />
+            ))}
+          </div>
         </div>
       </div>
 
